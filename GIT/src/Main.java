63c9af8f0958fad70b08e15bb1b8a39ee086a917
MiriKim <MiriKim@DESKTOP-O3F7OME>
2019-01-10 20:12:02 +0900

    ㅁㄹㅇㅁㄹ

--- GIT/src/Main.java
+++ GIT/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("첫번째수정내용");
-		int age=10;
+		
 		String name="유병승";
 		System.out.println("첫번쨰수정내용");
 		System.out.println("이거출충돌나나요");
